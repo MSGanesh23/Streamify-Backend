fd9f01e0c6b6c8e15c9a19d1e48088ca009aefce
MURALA SAI GANESH <163727585+MSGanesh23@users.noreply.github.com>
2025-09-23 19:04:33 +0530

    Update CorsConfig.java

--- src/main/java/com/demo/CorsConfig.java
+++ src/main/java/com/demo/CorsConfig.java
@@ -4,20 +4,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class CorsConfig {
 
     @Bean
-    public UrlBasedCorsConfigurationSource corsConfigurationSource() {
+    public CorsFilter corsFilter() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowCredentials(true);
-        configuration.addAllowedOrigin("http://localhost:8085"); // Allow your React app's origin
+        configuration.addAllowedOrigin("http://localhost:8085"); // browser access
+        configuration.addAllowedOrigin("http://frontend:8080");  // container access
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
-        return source;
+        return new CorsFilter(source);
     }
 }
