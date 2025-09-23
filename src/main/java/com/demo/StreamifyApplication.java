7d1833086b01c44488aa3440b7059f9fd8cf5423
MSGanesh23 <2300031569@kluniversity.in>
2025-09-23 19:51:20 +0530

    Changed backend to minimal

--- src/main/java/com/demo/StreamifyApplication.java
+++ src/main/java/com/demo/StreamifyApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RolebasedApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(RolebasedApplication.class, args);
-	}
-
+public class StreamifyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamifyApplication.class, args);
+    }
 }
